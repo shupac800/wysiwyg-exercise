b5a5063480d41562de753119316db322dbe6eb76
shupac800 <davidshuman@gmail.com>
2016-01-29 15:14:27 -0600

    working OK using innerHTML

---
+++ javascripts/wysiwyg.js
@@ -0,0 +1,40 @@
+
+var container = document.getElementById("container");
+
+var peeps = [
+{
+  title: "Spy",
+  name: "Klaus Fuchs",
+  bio: "As a mole at Los Alamos, Fuchs slipped the Soviets the atomic bomb secrets of the Manhattan Project.",
+  image: "http://spymuseum.com/wp-content/uploads/2012/12/portfolio-fuchs-klaus001.jpg",
+  lifespan: { birth: 1911, death: 1988 }
+},
+{
+  title: "Punk Rocker",
+  name: "Joey Ramone",
+  bio: "Front man of The Ramones, counterculture icon Joey Ramone took punk rock into the mainstream.",
+  image: "http://vignette3.wikia.nocookie.net/peel/images/5/51/09-joey-ramone.jpg/revision/latest",
+  lifespan: { birth: 1951, death: 2001 }
+},
+{
+  title: "Chanteuse",
+  name: "Edith Piaf",
+  bio: "Piaf was a French cabaret singer, songwriter and actress who became widely regarded as France's national chanteuse, as well as being one of France's greatest international stars.",
+  image: "http://www.singers.com/people/images/EdithPiaf.jpg",
+  lifespan: { birth: 1915, death: 1963 }
+}];
+
+///////////////
+
+peeps.forEach(function (p) {
+  container.innerHTML += `<div class="person">
+                            <header>${p.name}, ${p.title}</header>
+                            <section>
+                              <img src='${p.image}'>
+                              <p>${p.bio}</p>
+                            </section>
+                            <footer>
+                              <p>${p.lifespan.birth}-${p.lifespan.death}</p>
+                            </footer>
+                          </div>`;
+});
